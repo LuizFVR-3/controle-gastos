96ed65399dcdb6d82ee22eaa578cbe9ca4ec5353
LuizFVR-3 <luizvazrocha@gmail.com>
2024-08-27 21:55:57 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -6,13 +6,13 @@ const form = document.querySelector('#form')
 const inputTransactionName = document.querySelector('#text')
 const inputTransactionAmount = document.querySelector('#amount')
 
-// persistência de dados no localStorage
+
 const localStorageTransactions = JSON.parse(localStorage
     .getItem('transactions'))
 let transactions = localStorage
     .getItem('transactions') !== null ? localStorageTransactions : []
 
-// remover/apagar uma transação
+
 const removeTransaction = ID => {
     transactions = transactions.filter(transaction =>
         transaction.id !== ID)
@@ -20,13 +20,13 @@ const removeTransaction = ID => {
     init()
 }
 
-// adicionar transação no DOM
+
 const addTransationIntoDOM = ({ amount, name, id }) => {
 
     const operator = amount < 0 ? '-' : '+'
     const CSSClass = amount < 0 ? 'minus' : 'plus'
     const amountWithoutOperator = Math.abs(amount)
-    // Math.abs retorna o valor absoluto independente do sinal (+ / -)
+    
     const li = document.createElement('li')
 
     li.classList.add(CSSClass)
@@ -40,37 +40,37 @@ const addTransationIntoDOM = ({ amount, name, id }) => {
 
 }
 
-// valor total das despesas
+
+
 const getExpenses = transactionsAmounts => Math.abs(transactionsAmounts
     .filter(value => value < 0)
     .reduce((accumulator, value) => accumulator + value, 0))
     .toFixed(2)
 
-// valor total das receitas
+
 const getIncome = transactionsAmounts => transactionsAmounts
     .filter(value => value > 0)
     .reduce((accumulator, value) => accumulator + value, 0)
     .toFixed(2)
 
-// valor total
 const getTotal = transactionsAmounts => transactionsAmounts
     .reduce((accumulator, transaction) => accumulator + transaction, 0)
     .toFixed(2)
 
-// Atualiza o Controle Financeiro
+
 const updateBalenceValues = () => {
     const transactionsAmounts = transactions.map(({ amount }) => amount) // desestruturando o objeto
     const total = getTotal(transactionsAmounts)
     const income = getIncome(transactionsAmounts)
     const expense = getExpenses(transactionsAmounts)
     
-    // inserindo valor total no 'saldo atual'
+    
     balanceDisplay.textContent = `R$ ${total}`
 
-    // inserindo valor total das receitas em 'receitas'
+    
     incomeDisplay.textContent = `R$ ${income}`
 
-    // inserindo valor total das despesas em 'despesas'
+    
     expenseDisplay.textContent = `R$ ${expense}`
 }
 
@@ -82,12 +82,12 @@ const init = () => {
 
 init()
 
-// atualizando os dados no localStorage
+
 const updateLocalStorage = () => {
     localStorage.setItem('transactions', JSON.stringify(transactions))
 }
 
-// gerando ID aleatório para o formulário
+
 const generateID = () => Math.round(Math.random() * 1000)
 
 const addToTransactionsArray = (transactionName, transactionAmount) => {
@@ -121,5 +121,5 @@ const handleFormSubmit = event => {
     cleanInputs()
 }
 
-// formulário para inserir valores
+
 form.addEventListener('submit', handleFormSubmit)
